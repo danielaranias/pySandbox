f73716b024298cbbd342c22899ce9b1491c6d4eb
daniel aranias <danielaranias@daniels-Air.lan>
2020-03-27 20:14:41 +0300

    adding main

---
+++ main.py
@@ -0,0 +1,15 @@
+import StringTool
+
+def main():
+    print('hello word')
+    
+    #creating the StringTool object
+    str_tool = StringTool.StringTool()
+    str_tool.findAllStrPos('Here IS a word and another word. the end od the word','word')
+        
+        
+
+
+
+if __name__ == "__main__":
+    main()
