5d06f692e8508494d8aa9055ca50c9625296110c
daniel aranias <danielaranias@daniels-Air.lan>
2020-03-27 18:25:00 +0300

    Creating String Tool class to manipulate strings

---
+++ testModule2.py
@@ -0,0 +1,19 @@
+
+
+def printStr(strToPrint):
+    """ Here I am writing the function description
+    and that's it"""
+
+    print(strToPrint)
+
+    
+text1 = 'Here is a text' + ' and'
+text2 = 'dd'
+text3 = """ Here I am writing the function description
+    and that's it"""
+
+
+print(text1[:-1])
+
+
+
