f6a4f6bcb8b8b19398ef62f1461db67b999c4996
daniel aranias <danielaranias@daniels-Air.lan>
2020-03-28 15:07:55 +0300

    while instead of for

--- StringTool.py
+++ StringTool.py
@@ -9,25 +9,25 @@ class StringTool:
         not_found_ch = 'www'.find('e')
 
         print('The entered string is:',mainStr)
-        #converting the main str to lower case string
+        #converting the main str to lower case string using casefold for more aggressive lower action
         #in order to work with no case sensative"
-        mainStr = mainStr.lower()
-        strToFind = strToFind.lower()
+        mainStr = mainStr.casefold()
+        strToFind = strToFind.casefold()
+        found_abs_pos = not_found_ch
         next_pos = 0
 
-
-        #run on the main str, finding the next position that strToFind is found
-        for x in range (next_pos,len(mainStr)):
-
-            found_abs_pos = mainStr[next_pos:].find(strToFind)
+        #find the first posution of the dub str
+        found_abs_pos = mainStr[next_pos:].find(strToFind)
+        #run on the main str, untill nothing has found
+        while found_abs_pos != not_found_ch:
             #since we found the absolute position we should add it to the last position we found
             next_pos += found_abs_pos
+            #adding the position to the list
+            all_subStr_positions_list.append(next_pos)
+            #we need to pass over the word that we found
+            next_pos += len(strToFind)
+            #find the first posution of the dub str
+            found_abs_pos = mainStr[next_pos:].find(strToFind)
 
-            if found_abs_pos == not_found_ch:
-                return all_subStr_positions_list
-            else:
-                #adding the position to the list
-                all_subStr_positions_list.append(next_pos)
-                #we need to pass the word that we founs
-                next_pos += len(strToFind)
+        return all_subStr_positions_list
                 
